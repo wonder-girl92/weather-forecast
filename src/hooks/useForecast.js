009598d851c675ad2f169a8118e84094a2e0e660
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2022-07-16 13:59:22 +0300

    url changing

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -6,7 +6,7 @@ export const useForecast = (city) => {
   useEffect(() => {
     if (city !== null) {
       fetch(
-        `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no&lang=ru`
+        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no&lang=ru`
       )
         .then((res) => res.json())
         .then(setData);
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -4,7 +4,7 @@ import { API_KEY } from '../settings';
 export const useWeather = (city) => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=ru`)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=ru`)
       .then((res) => res.json())
       .then((fetchedData) => {
         if (fetchedData.location) {
